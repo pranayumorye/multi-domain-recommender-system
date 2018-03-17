ad22cc7976d190c18d60c4727ba1e462d6cdf924
countnightlock <vivekcmaurya30@gmail.com>
2018-03-17 20:01:17 +0530

    Added csv-pushing capability to regression-prep

--- regression_prep.py
+++ regression_prep.py
@@ -25,31 +25,53 @@ collab_simil = np.loadtxt("collab_simil.txt", dtype = np.float16)
 def get_length_simil(idx1, idx2):
     return (1 - abs(df.iloc[idx1]["length_rep"] - df.iloc[idx2]["length_rep"])/2)
 
+
 # for age simil
 def get_age_simil(idx1, idx2):
     return (1- abs(df.iloc[idx1]["age"] - df.iloc[idx2]["age"])/3)
 
+
 # for tfidf simil
 def get_tfidf_simil(idx1, idx2):
     return (tfidf_simil[idx1, idx2])
+
 
 # for output vector from collaborative similarities
 def get_collab_simil(idx1, idx2):
     return (collab_simil[idx1, idx2])
 
 
-
 for pair in itertools.product(range(0, 100), repeat=2):
     book_id_pairs.append(pair)
 
+
 for idx1, idx2 in book_id_pairs[:10]:
+    print(df.iloc[idx1]["original_title"], "|" , df.iloc[idx2]["original_title"])
     print(get_collab_simil(idx1, idx2), get_tfidf_simil(idx1, idx2), get_age_simil(idx1, idx2), get_length_simil(idx1, idx2))
 
 # Storing in a csv
-regr_data = pd.DataFrame(columns = ["id_pair","tfidf_simil", "age_simil", "length_simil", "collab_simil"])
+columns = ["id_pair","tfidf_simil", "age_simil", "length_simil", "collab_simil"]
+regr_data = pd.DataFrame(columns=columns)
 
+tfidf_simil_list = []
+age_simil_list = []
+length_simil_list = []
+collab_simil_list = []
 
 for idx1, idx2 in book_id_pairs:
-    
+    tfidf_simil_list.append(get_tfidf_simil(idx1, idx2))
+    age_simil_list.append(get_age_simil(idx1, idx2))
+    length_simil_list.append(get_length_simil(idx1, idx2))
+    collab_simil_list.append(get_collab_simil(idx1, idx2))
 
-regr_data.to_csv("regr_data.csv", index = False)
+regr_data = pd.DataFrame(data={
+    "id_pair": book_id_pairs,
+    "tfidf_simil": tfidf_simil_list,
+    "age_simil": age_simil_list,
+    "length_simil": length_simil_list,
+    "collab_simil": collab_simil_list
+})
+
+print(regr_data.head())
+
+regr_data.to_csv("regr_data.csv", index = False, columns=columns)
