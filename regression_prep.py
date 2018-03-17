b5db6299d6bb27d61cfed99e361073b6bf2e73a8
Pranay Morye <pranay.morye@gmail.com>
2018-03-17 19:16:03 +0530

    incomplete regr_prep

--- regression_prep.py
+++ regression_prep.py
@@ -17,13 +17,39 @@ df = pd.read_csv("./books_final.csv", **params)
 df = df[:100]
 book_id_pairs = []
 
+tfidf_simil = np.loadtxt("tfidf_simil.txt", dtype = np.float16)
+collab_simil = np.loadtxt("collab_simil.txt", dtype = np.float16)
 
-def get_length_reps(idx1, idx2):
-    return df.iloc[idx1]["length_rep"], df.iloc[idx2]["length_rep"]
+
+# for length simil
+def get_length_simil(idx1, idx2):
+    return (1 - abs(df.iloc[idx1]["length_rep"] - df.iloc[idx2]["length_rep"])/2)
+
+# for age simil
+def get_age_simil(idx1, idx2):
+    return (1- abs(df.iloc[idx1]["age"] - df.iloc[idx2]["age"])/3)
+
+# for tfidf simil
+def get_tfidf_simil(idx1, idx2):
+    return (tfidf_simil[idx1, idx2])
+
+# for output vector from collaborative similarities
+def get_collab_simil(idx1, idx2):
+    return (collab_simil[idx1, idx2])
+
 
 
 for pair in itertools.product(range(0, 100), repeat=2):
     book_id_pairs.append(pair)
 
 for idx1, idx2 in book_id_pairs[:10]:
-    print(get_length_reps(idx1, idx2))
+    print(get_collab_simil(idx1, idx2), get_tfidf_simil(idx1, idx2), get_age_simil(idx1, idx2), get_length_simil(idx1, idx2))
+
+# Storing in a csv
+regr_data = pd.DataFrame(columns = ["id_pair","tfidf_simil", "age_simil", "length_simil", "collab_simil"])
+
+
+for idx1, idx2 in book_id_pairs:
+    
+
+regr_data.to_csv("regr_data.csv", index = False)
--- test_tfidf.py
+++ test_tfidf.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
-df = pd.read_csv("./combined_final.csv")
+df = pd.read_csv("./books_final.csv")
 dfc = df
 
 tf = TfidfVectorizer(analyzer='word',ngram_range=(1,3),min_df=0,stop_words='english')
@@ -11,19 +12,25 @@ tfidf_matrix = tf.fit_transform(dfc['plot'].values.astype('U'))
 # print (tfidf_matrix.shape)
 cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
 
+cosine_similarities = cosine_similarities[0:100]
+cosine_similarities = cosine_similarities.T
+cosine_similarities = cosine_similarities[0:100]
+print(cosine_similarities.shape)
 
-for idx, row in dfc.iterrows():
-    similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
-    if row['isbook'] == 1:
-        similar_items = [(round(cosine_similarities[idx][i],2), dfc['title'][i])
-                        for i in similar_indices if dfc['isbook'][i] == 0]
-    else:
-        similar_items = [(round(cosine_similarities[idx][i],2), dfc['title'][i])
-                        for i in similar_indices if dfc['isbook'][i] == 0]
-    if len(similar_items) >= 10:
-        similar_items = similar_items[:10]
-    print(row['title'])
-    print(similar_items)
+np.savetxt("tfidf_simil.txt", cosine_similarities)
+
+# for idx, row in dfc.iterrows():
+#     similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
+#     if row['isbook'] == 1:
+#         similar_items = [(round(cosine_similarities[idx][i],2), dfc['title'][i])
+#                         for i in similar_indices if dfc['isbook'][i] == 0]
+#     else:
+#         similar_items = [(round(cosine_similarities[idx][i],2), dfc['title'][i])
+#                         for i in similar_indices if dfc['isbook'][i] == 0]
+#     if len(similar_items) >= 10:
+#         similar_items = similar_items[:10]
+#     print(row['title'])
+#     print(similar_items)
 
 # print (cosine_similarities)
         
