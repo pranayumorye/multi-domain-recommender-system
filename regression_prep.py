cb2e02efe689cc7584489fde07156f354272a1dc
countnightlock <vivekcmaurya30@gmail.com>
2018-03-18 11:16:37 +0530

    X, y matrix csv ready

--- regression_prep.py
+++ regression_prep.py
@@ -17,9 +17,9 @@ df = pd.read_csv("./books_final.csv", **params)
 df = df[:100]
 book_id_pairs = []
 
-tfidf_simil = np.loadtxt("tfidf_simil.txt", dtype = np.float16)
-collab_simil = np.loadtxt("collab_simil.txt", dtype = np.float16)
-
+tfidf_simil = np.loadtxt("tfidf_simil.txt", dtype=np.float16)
+collab_simil = np.loadtxt("collab_simil.txt", dtype=np.float16)
+genre_simil = np.loadtxt("genre_simil_matrix.txt", dtype=np.float16)
 
 # for length simil
 def get_length_simil(idx1, idx2):
@@ -41,26 +41,28 @@ def get_collab_simil(idx1, idx2):
     return (collab_simil[idx1, idx2])
 
 
+def get_genre_simil(idx1, idx2):
+    return genre_simil[idx1, idx2]
+
+
 for pair in itertools.product(range(0, 100), repeat=2):
     book_id_pairs.append(pair)
 
 
-for idx1, idx2 in book_id_pairs[:10]:
-    print(df.iloc[idx1]["original_title"], "|" , df.iloc[idx2]["original_title"])
-    print(get_collab_simil(idx1, idx2), get_tfidf_simil(idx1, idx2), get_age_simil(idx1, idx2), get_length_simil(idx1, idx2))
-
 # Storing in a csv
-columns = ["id_pair","tfidf_simil", "age_simil", "length_simil", "collab_simil"]
+columns = ["id_pair","tfidf_simil", "genre_simil", "age_simil", "length_simil", "collab_simil"]
 regr_data = pd.DataFrame(columns=columns)
 
 tfidf_simil_list = []
 age_simil_list = []
 length_simil_list = []
+genre_simil_list = []
 collab_simil_list = []
 
 for idx1, idx2 in book_id_pairs:
     tfidf_simil_list.append(get_tfidf_simil(idx1, idx2))
     age_simil_list.append(get_age_simil(idx1, idx2))
+    genre_simil_list.append(get_genre_simil(idx1, idx2))
     length_simil_list.append(get_length_simil(idx1, idx2))
     collab_simil_list.append(get_collab_simil(idx1, idx2))
 
@@ -69,7 +71,8 @@ regr_data = pd.DataFrame(data={
     "tfidf_simil": tfidf_simil_list,
     "age_simil": age_simil_list,
     "length_simil": length_simil_list,
-    "collab_simil": collab_simil_list
+    "genre_simil": genre_simil_list,
+    "collab_simil": collab_simil_list,
 })
 
 print(regr_data.head())
